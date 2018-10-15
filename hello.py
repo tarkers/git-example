5850ba6f19fb3eed13c84bde684a14a31350dcea
tarkers <big.shrimp1117@gamil.com>
2018-10-16 01:16:57 +0800

    work done

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 # hi, this is comment 
+=======
+# hi ,this is a comment
+>>>>>>> 4737fb8dcb1b8d9f58ff403883c879e04e02f753
 print('hello world')
 print('new to the github')
 print('elco')
