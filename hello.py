fcf446f304b732c9a4de803e823438fcc8f72a23
tarkers <big.shrimp1117@gamil.com>
2018-10-16 01:41:11 +0800

    ubstract

--- hello.py
+++ hello.py
@@ -9,8 +9,4 @@
 >>>>>>> 4737fb8dcb1b8d9f58ff403883c879e04e02f753
 print('hello world')
 print('new to the github')
-print('elco')
-print('the'
-print('to take the world')
-
-whats wrong
+print('im the king of the world')
